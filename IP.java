101ac1156e8a8dc16e72e8e7e2af5809f8e92b2e
ilsanbao <ilsanboo@gmail.com>
2014-11-19 14:12:25 +0800

    update code

--- IP.java
+++ IP.java
@@ -4,31 +4,85 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 class IP {
 
     public static void main(String[] args){
-
         IP.load("H:\\loveapp\\codebase\\17mon\\17monipdb.dat");
 
         System.out.println(Arrays.toString(IP.find("8.8.8.8")));
-        System.out.println(Arrays.toString(IP.find("118.28.38.58")));
         System.out.println(Arrays.toString(IP.find("255.255.255.255")));
     }
 
+    public static boolean enableFileWatch = false;
+
     private static int offset;
     private static int[] index = new int[256];
-
     private static ByteBuffer dataBuffer;
     private static ByteBuffer indexBuffer;
+    private static Long lastModifyTime = 0L;
+    private static File ipFile ;
+    private static ReentrantLock lock = new ReentrantLock();
 
     public static void load(String filename) {
-        File ipFile = new File(filename);
+        ipFile = new File(filename);
+        load();
+        if (enableFileWatch)
+        {
+            watch();
+        }
+    }
 
-        final int fileLength = Long.valueOf(ipFile.length()).intValue();
-        dataBuffer = ByteBuffer.allocate(fileLength);
+    public static String[] find(String ip) {
+        int ip_prefix_value = new Integer(ip.substring(0, ip.indexOf(".")));
+        long ip2long_value  = ip2long(ip);
+        int start = index[ip_prefix_value];
+        int max_comp_len = offset - 1028;
+        long index_offset = -1;
+        int index_length = -1;
+        byte b = 0;
+        for (start = start * 8 + 1024; start < max_comp_len; start += 8) {
+            if (int2long(indexBuffer.getInt(start)) >= ip2long_value) {
+                index_offset = bytesToLong(b, indexBuffer.get(start + 6), indexBuffer.get(start + 5), indexBuffer.get(start + 4));
+                index_length = 0xFF & indexBuffer.get(start + 7);
+                break;
+            }
+        }
+
+        lock.lock();
+        dataBuffer.position(offset + (int) index_offset - 1024);
+        byte[] areaBytes = new byte[index_length];
+        dataBuffer.get(areaBytes, 0, index_length);
+        lock.unlock();
+
+        return new String(areaBytes).split("\t");
+    }
+
+    private static void watch() {
+
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+
+                long time = ipFile.lastModified();
+                if (time > lastModifyTime) {
+                    lastModifyTime = time;
+                    load();
+                }
+            }
+        }, 1000L, 5000L, TimeUnit.MILLISECONDS);
+    }
+
+    private static void load() {
+        lastModifyTime = ipFile.lastModified();
         FileInputStream fin = null;
         try {
+            lock.lock();
+            final int fileLength = Long.valueOf(ipFile.length()).intValue();
+            dataBuffer = ByteBuffer.allocate(fileLength);
             fin = new FileInputStream(ipFile);
             int readBytesLength;
             byte[] chunk = new byte[4096];
@@ -50,49 +104,21 @@ class IP {
             }
             indexBuffer.order(ByteOrder.BIG_ENDIAN);
         } catch (IOException ioe) {
-            //
         } finally {
             try {
                 if (fin != null) {
                     fin.close();
                 }
             } catch (IOException e){
-                //
             }
+            lock.unlock();
         }
-    }
-
-    public static String[] find(String ip) {
-        int ip_prefix_value = new Integer(ip.substring(0, ip.indexOf(".")));
-        long ip2long_value  = ip2long(ip);
-        int start = index[ip_prefix_value];
-        int max_comp_len = offset - 1028;
-        long tmpInt;
-        long index_offset = -1;
-        int index_length = -1;
-        byte b = 0;
-        for (start = start * 8 + 1024; start < max_comp_len; start += 8) {
-            tmpInt = int2long(indexBuffer.getInt(start));
-            if (tmpInt >= ip2long_value) {
-                index_offset = bytesToLong(b, indexBuffer.get(start + 6), indexBuffer.get(start + 5), indexBuffer.get(start + 4));
-                index_length = 0xFF & indexBuffer.get(start + 7);
-                break;
-            }
-        }
-
-        byte[] areaBytes;
-        synchronized (dataBuffer) {
-            dataBuffer.position(offset + (int) index_offset - 1024);
-            areaBytes = new byte[index_length];
-            dataBuffer.get(areaBytes, 0, index_length);
-        }
-
-        return new String(areaBytes).split("\t");
     }
 
     private static long bytesToLong(byte a, byte b, byte c, byte d) {
         return int2long((((a & 0xff) << 24) | ((b & 0xff) << 16) | ((c & 0xff) << 8) | (d & 0xff)));
     }
+
     private static int str2Ip(String ip)  {
         String[] ss = ip.split("\\.");
         int a, b, c, d;
